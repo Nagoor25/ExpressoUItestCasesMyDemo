2dc218e2b831cd8e37fca8ed7d8f27e74a22d364
Nagoor25 <nagoorvali25@gmail.com>
2022-02-16 17:18:03 +0530

    SimpleMainActivityUI&Navigation_mainActivity_ui_tests

--- app/src/androidTest/java/com/example/expressodemo/ui/MainActivityTest.kt
+++ app/src/androidTest/java/com/example/expressodemo/ui/MainActivityTest.kt
@@ -1,6 +1,5 @@
-<<<<<<< HEAD
 package com.example.expressodemo.ui
-
+import org.junit.Test
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.Espresso.pressBack
@@ -9,57 +8,55 @@ import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import com.example.expressodemo.R
-=======
-package com.example.expressodemo
 
-import androidx.test.core.app.ActivityScenario
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.*
-import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
-import com.example.expressodemo.ui.MainActivity
->>>>>>> test_suites
-import org.junit.Test
-import org.junit.runner.RunWith
 
-@RunWith(AndroidJUnit4ClassRunner::class)
-class MainActivityTest{
-<<<<<<< HEAD
-
+class MainActivityTest {
+    //Header
+    //first simple Main Activity ui tests
     @Test
-    fun test_navigationToSecondActivity(){
-        val activityScenario=ActivityScenario.launch(MainActivity::class.java)
-        onView(withId(R.id.button_next_activity)).perform(click())
-        onView(withId(R.id.secondary)).check(matches(isDisplayed()))
-    }
-    @Test
-    fun test_backPress_to_mainActivity(){
-        val activityScenario=ActivityScenario.launch(MainActivity::class.java)
-        onView(withId(R.id.button_next_activity)).perform(click())
-        onView(withId(R.id.secondary)).check(matches(isDisplayed()))
-     //   onView(withId(R.id.button_back)).perform(click()) // method1
-        pressBack()// method 2
-        onView(withId(R.id.main)).check(matches(isDisplayed()))
-=======
-    @Test
-    fun testActivity_isInView(){
-        val activityScenario=ActivityScenario.launch(MainActivity::class.java)
+    fun testActivity_isInView() {
+        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
         onView(withId(R.id.main)).check(matches(isDisplayed()))// Method1 to view is visible
     }
+
     @Test
-    fun testNextButton_isInView_or_visible(){
-        val activityScenario=ActivityScenario.launch(MainActivity::class.java)
+    fun testNextButton_isInView_or_visible() {
+        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
         //  onView(withId(R.id.button_next_activity)).check(matches(isDisplayed()))//method 1
-        onView(withId(R.id.button_next_activity)).
-        check(matches(withEffectiveVisibility(Visibility.VISIBLE))) // method2
+        onView(withId(R.id.button_next_activity)).check(matches(withEffectiveVisibility(Visibility.VISIBLE))) // method2
         onView(withId(R.id.activity_main_title)).check(matches(isDisplayed()))
     }
+
     @Test
-    fun testIsTitle_isDisplayed(){
-        val activityScenario=ActivityScenario.launch(MainActivity::class.java)
-        onView(withId(R.id.activity_main_title)).check(matches(withText(R.string.text_mainactivity
-        )))
->>>>>>> test_suites
+    fun testIsTitle_isDisplayed() {
+        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
+        onView(withId(R.id.activity_main_title)).check(
+            matches(
+                withText(
+                    R.string.text_mainactivity
+                )
+            )
+        )
     }
 
+    //
+// 3 Navigation MainActivity ui tests
+    @Test
+    fun test_navigationToSecondActivity() {
+
+        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
+        onView(withId(R.id.button_next_activity)).perform(click())
+        onView(withId(R.id.secondary)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun test_backPress_to_mainActivity() {
+        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
+        onView(withId(R.id.button_next_activity)).perform(click())
+        onView(withId(R.id.secondary)).check(matches(isDisplayed()))
+        //   onView(withId(R.id.button_back)).perform(click()) // method1
+        pressBack()// method 2
+        onView(withId(R.id.main)).check(matches(isDisplayed()))
+
+    }
 }
