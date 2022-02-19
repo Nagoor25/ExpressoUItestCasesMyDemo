0f860a1eb6ab5f68943eda77090ae2c54be0445e
Nagoor25 <nagoorvali25@gmail.com>
2022-02-19 17:15:29 +0530

    7_fragments TestSuite

---
+++ app/src/androidTest/java/com/example/expressodemo/ui/Fragment/MovieFragmentTestSuite.kt
@@ -0,0 +1,11 @@
+package com.example.expressodemo.ui.Fragment
+
+import org.junit.runner.RunWith
+import org.junit.runners.Suite
+
+@RunWith(Suite::class)
+@Suite.SuiteClasses(MovieDetailFragmentTest::class,
+                  DirectorsFragmentTest::class,
+                   StarActorsFragmentTest::class)
+class MovieFragmentTestSuite {
+}
